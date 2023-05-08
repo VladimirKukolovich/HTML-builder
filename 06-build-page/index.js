92c34d33c7ea63ba289a5e4b7231d7ac9fd2f1c1
Vladimir <vladimirkukolovich@gmail.com>
2023-05-08 23:42:37 +0300

    fixL: dd 1-5

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 let fold = path.join(__dirname, 'styles');
-let components = path.join(__dirname, 'styles');
-const stream = fs.ReadStream( path.join(__dirname, 'template.html'), 'utf-8');
+let components = path.join(__dirname, 'components');
+// const stream = fs.ReadStream( path.join(__dirname, 'template.html'), 'utf-8');
 let newDirPath = path.join(__dirname, 'project-dist');
 
 function buildPage() {
@@ -15,22 +15,34 @@ function buildPage() {
       fs.mkdir(newDirPath, (err) => {
         if (err) throw err;
       });
-      stream.on('data', chunk => console.log(chunk));
+      //   stream.on('data', chunk => console.log(chunk));
 
       //   fs.appendFile(path.resolve(__dirname,'project-dist/index.html'), '', (error) => {
       //     if(error)console.log(error);
       //   });
+      fs.readFile('06-build-page/template.html', (err, data) =>{
+        if(err) throw err;
 
+        console.log(data);
+      });
       fs.readdir(components, {withFileTypes: true}, (err, files)=> {
+        // console.log(files);
+
         if(err) throw err;
         const recStreamHtml = fs.createWriteStream(path.resolve(__dirname, 'project-dist', 'index.html'));
-        files.forEach((i) => {
-          if(i.isFile() && path.extname(i.name) === '.html' ){
-            console.log(i.chunk);
-            let streamRead = fs.createReadStream(path.join(components, i.name));
-            streamRead.pipe(recStreamHtml);
-          }
-        });
+        let stream1 = fs.createReadStream(path.join(__dirname, 'template.html'));
+        // console.log(recStreamHtml);
+        console.log(!files);
+
+        stream1.pipe(recStreamHtml);
+        // files.forEach((i) => {
+        //   console.log(i);
+        //   if(i.isFile() && path.extname(i.name) === '.html' ){
+        //     let streamRead = fs.createReadStream(path.join(components, i.name));
+        //     console.log(streamRead);
+        //     streamRead.pipe(recStreamHtml);
+        //   }
+        // });
       });
  
 
